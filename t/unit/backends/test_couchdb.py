603ec0fb9135ba7073e76eabc1adf0d297cc72b3
xcr <xcr@xcr>
2026-06-23 22:16:02 +0800

    Kusanagi238__diff__id_231

--- t/unit/backends/test_couchdb.py
+++ t/unit/backends/test_couchdb.py
@@ -15,15 +15,14 @@ try:
 except ImportError:
     pycouchdb = None
 
-COUCHDB_CONTAINER = 'celery_container'
+COUCHDB_CONTAINER = "celery_container"
 
-pytest.importorskip('pycouchdb')
+pytest.importorskip("pycouchdb")
 
 
 class test_CouchBackend:
-
     def setup_method(self):
-        self.Server = self.patching('pycouchdb.Server')
+        self.Server = self.patching("pycouchdb.Server")
         self.backend = CouchBackend(app=self.app)
 
     def test_init_no_pycouchdb(self):
@@ -50,40 +49,40 @@ class test_CouchBackend:
         x = CouchBackend(app=self.app)
         x._connection = Mock()
         get = x._connection.get = MagicMock()
-        assert x.get('1f3fab') == get.return_value['value']
-        x._connection.get.assert_called_once_with('1f3fab')
+        assert x.get("1f3fab") == get.return_value["value"]
+        x._connection.get.assert_called_once_with("1f3fab")
 
     def test_get_non_existent_key(self):
         x = CouchBackend(app=self.app)
         x._connection = Mock()
         get = x._connection.get = MagicMock()
         get.side_effect = pycouchdb.exceptions.NotFound
-        assert x.get('1f3fab') is None
-        x._connection.get.assert_called_once_with('1f3fab')
+        assert x.get("1f3fab") is None
+        x._connection.get.assert_called_once_with("1f3fab")
 
-    @pytest.mark.parametrize("key", ['1f3fab', b'1f3fab'])
+    @pytest.mark.parametrize("key", ["1f3fab", b"1f3fab"])
     def test_set(self, key):
         x = CouchBackend(app=self.app)
         x._connection = Mock()
 
-        x._set_with_state(key, 'value', states.SUCCESS)
+        x._set_with_state(key, "value", states.SUCCESS)
 
-        x._connection.save.assert_called_once_with({'_id': '1f3fab',
-                                                    'value': 'value'})
+        x._connection.save.assert_called_once_with({"_id": "1f3fab", "value": "value"})
 
-    @pytest.mark.parametrize("key", ['1f3fab', b'1f3fab'])
+    @pytest.mark.parametrize("key", ["1f3fab", b"1f3fab"])
     def test_set_with_conflict(self, key):
         x = CouchBackend(app=self.app)
         x._connection = Mock()
         x._connection.save.side_effect = (pycouchdb.exceptions.Conflict, None)
         get = x._connection.get = MagicMock()
 
-        x._set_with_state(key, 'value', states.SUCCESS)
+        x._set_with_state(key, "value", states.SUCCESS)
 
-        x._connection.get.assert_called_once_with('1f3fab')
-        x._connection.get('1f3fab').__setitem__.assert_called_once_with(
-            'value', 'value')
-        x._connection.save.assert_called_with(get('1f3fab'))
+        x._connection.get.assert_called_once_with("1f3fab")
+        x._connection.get("1f3fab").__setitem__.assert_called_once_with(
+            "value", "value"
+        )
+        x._connection.save.assert_called_with(get("1f3fab"))
         assert x._connection.save.call_count == 2
 
     def test_delete(self):
@@ -99,24 +98,26 @@ class test_CouchBackend:
         mocked_delete = x._connection.delete = Mock()
         mocked_delete.return_value = None
         # should return None
-        assert x.delete('1f3fab') is None
-        x._connection.delete.assert_called_once_with('1f3fab')
+        assert x.delete("1f3fab") is None
+        x._connection.delete.assert_called_once_with("1f3fab")
 
-    def test_backend_by_url(self, url='couchdb://myhost/mycoolcontainer'):
+    def test_backend_by_url(self, url="couchdb://myhost/mycoolcontainer"):
         from celery.backends.couchdb import CouchBackend
+
         backend, url_ = backends.by_url(url, self.app.loader)
         assert backend is CouchBackend
         assert url_ == url
 
     def test_backend_params_by_url(self):
-        url = 'couchdb://johndoe:mysecret@myhost:123/mycoolcontainer'
+        url = "couchdb://johndoe:mysecret@myhost:123/mycoolcontainer"
         with self.Celery(backend=url) as app:
             x = app.backend
-            assert x.container == 'mycoolcontainer'
-            assert x.host == 'myhost'
-            assert x.username == 'johndoe'
-            assert x.password == 'mysecret'
+            assert x.container == "mycoolcontainer"
+            assert x.host == "myhost"
+            assert x.username == "johndoe"
+            assert x.password == "mysecret"
             assert x.port == 123
+
 
 class CouchSessionMock:
     """
@@ -125,14 +126,15 @@ class CouchSessionMock:
 
     _store = {}
 
-    def request(self, method, url, stream=False, data=None, params=None,
-                 headers=None, **kw):
+    def request(
+        self, method, url, stream=False, data=None, params=None, headers=None, **kw
+    ):
         tid = urlparse(url).path.split("/")[-1]
 
         response = Mock()
         response.headers = {"content-type": "application/json"}
         response.status_code = 200
-        response.content = b''
+        response.content = b""
 
         if method == "GET":
             if tid not in self._store:
@@ -140,7 +142,9 @@ class CouchSessionMock:
             response.content = self._store.get(tid)
         elif method == "PUT":
             self._store[tid] = data
-            response.content = str_to_bytes(f'{{"ok":true,"id":"{tid}","rev":"1-revid"}}')
+            response.content = str_to_bytes(
+                f'{{"ok":true,"id":"{tid}","rev":"1-revid"}}'
+            )
         elif method == "HEAD":
             if tid not in self._store:
                 return self._not_found_response()
@@ -149,9 +153,13 @@ class CouchSessionMock:
             if tid not in self._store:
                 return self._not_found_response()
             del self._store[tid]
-            response.content = str_to_bytes(f'{{"ok":true,"id":"{tid}","rev":"1-revid"}}')
+            response.content = str_to_bytes(
+                f'{{"ok":true,"id":"{tid}","rev":"1-revid"}}'
+            )
         else:
-            raise NotImplemented(f"CouchSessionMock.request() does not handle {method} method")
+            raise NotImplementedError(
+                f"CouchSessionMock.request() does not handle {method} method"
+            )
 
         return response
 
@@ -166,7 +174,9 @@ class CouchSessionMock:
 class test_CouchBackend_result:
     def setup_method(self):
         self.backend = CouchBackend(app=self.app)
-        resource = pycouchdb.resource.Resource("resource-url", session=CouchSessionMock())
+        resource = pycouchdb.resource.Resource(
+            "resource-url", session=CouchSessionMock()
+        )
         self.backend._connection = pycouchdb.client.Database(resource, "container")
 
     def test_get_set_forget(self):
@@ -190,10 +200,11 @@ class test_CouchBackend_result:
     def test_mark_as_retry(self):
         tid = uuid()
         try:
-            raise KeyError('foo')
+            raise KeyError("foo")
         except KeyError as exception:
             import traceback
-            trace = '\n'.join(traceback.format_stack())
+
+            trace = "\n".join(traceback.format_stack())
             self.backend.mark_as_retry(tid, exception, traceback=trace)
             assert self.backend.get_state(tid) == states.RETRY
             assert isinstance(self.backend.get_result(tid), KeyError)
@@ -202,10 +213,11 @@ class test_CouchBackend_result:
     def test_mark_as_failure(self):
         tid = uuid()
         try:
-            raise KeyError('foo')
+            raise KeyError("foo")
         except KeyError as exception:
             import traceback
-            trace = '\n'.join(traceback.format_stack())
+
+            trace = "\n".join(traceback.format_stack())
             self.backend.mark_as_failure(tid, exception, traceback=trace)
             assert self.backend.get_state(tid) == states.FAILURE
             assert isinstance(self.backend.get_result(tid), KeyError)
